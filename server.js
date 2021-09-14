f7a92729c3ec1704ee77062eac9afcc33b3f09a6
Myles-the-Coder <76969330+Myles-the-Coder@users.noreply.github.com>
2021-09-14 15:36:22 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,41 @@
+import express, { response } from 'express';
+import bcrypt from 'bcrypt-nodejs';
+import cors from 'cors';
+import pkg from 'knex';
+
+import {handleRegister} from './controllers/register.js'
+import {handleSignIn} from './controllers/signin.js'
+import {handleProfileGet} from './controllers/profile.js'
+import {handleImage} from './controllers/image.js'
+
+const { knex } = pkg;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const PASSWORD = process.env.PASSWORD
+
+const db = knex({
+	client: 'pg',
+	connection: {
+		host: '127.0.0.1',
+		user: 'postgres',
+		password: PASSWORD,
+		database: 'smart-brain',
+	},
+});
+
+app.get('/', (req, res) => {
+	res.send('Success');
+});
+
+app.post('/signin', (req, res) => {handleSignIn(req, res, db, bcrypt)});
+app.post('/register', (req, res) => {handleRegister(req, res, db, bcrypt)});
+app.get('/profile/:id', (req, res) => {handleProfileGet(req, res, db)});
+app.put('/image', (req, res) => {handleImage(req, res, db)})
+app.post('/imageurl', (req, res) => {handleApiCall(req, res, db)});
+
+const PORT = process.env.PORT
+
+app.listen(PORT);
